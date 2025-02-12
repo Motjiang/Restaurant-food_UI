b5f22bc5a66a6aea7fa55a014f932ee4ef02ad85
Motjiang <motjiangbrilliant@gmail.com>
2025-02-12 23:55:32 +0200

    Added the home component which is the about us

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -5,9 +5,29 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   template: `
-    <p>
-      home works!
-    </p>
+    <div class="section__container header__container" id="home">
+        <div class="header__image">
+          <img src="assets/header.png" alt="header" />
+        </div>
+        <div class="header__content">
+          <div class="header__tag">
+            More than Faster
+            <img src="assets/delivery-bike.png" alt="header tag" />
+          </div>
+          <h1>Be The Fastest In Delivering Your <span>Food</span></h1>
+          <p class="section__description">
+            Our job is to filling your tummy with delicious food and with fast
+            and free delivery.
+          </p>
+          <div class="header__btns">
+            <button class="btn">Get Started</button>
+            <a href="#">
+              <span><i class="ri-play-fill"></i></span>
+              Watch Video
+            </a>
+          </div>
+        </div>
+      </div>
   `,
   styleUrl: './home.component.css'
 })
